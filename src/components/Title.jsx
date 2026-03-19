239ce5bb4072c5c7148e15207e7018eda28c35f9
Ashish Kumar Jangid <your-email@example.com>
2026-03-19 16:23:57 +0530

    Speaker 9

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -5,7 +5,7 @@ export default function Title() {
   return (
     <div className="title-container">
       <div className="title-main">
-        <span className="title-bold">IIT(BHU)-NIMS WORKSHOP</span><br />
+        <span className="title-bold">IIT(BHU)-NIMS JOINT WORKSHOP</span><br />
         <span className="title-on">ON</span><br />
         <span className="title-bold">TRENDS IN QUANTUM MATERIALS</span>
       </div>
